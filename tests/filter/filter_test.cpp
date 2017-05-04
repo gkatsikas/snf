690343c26c7dfbab6f49e02bfb45ccc6b95caba7
Georgios Katsikas <george.dit@gmail.com>
2017-05-04 18:24:29 +0200

    Updated filter test with separation between 2 sub-tests

--- tests/filter/filter_test.cpp
+++ tests/filter/filter_test.cpp
@@ -28,56 +28,91 @@
 #include "../../src/shared/helpers.hpp"
 #include "../../src/traffic_class_builder/filter.hpp"
 
+void create_filters();
+void intersect_filters();
+
+// Initialize logger
+Logger test_log(__FILE__);
+
 int
 main()
 {
-	// Initialize logger
-	Logger test_log(__FILE__);
-
 	info_chatter(test_log, "Filter test started");
 
-	Filter f1(ip_dst, aton("200.0.0.2"));
-	info_chatter(test_log, "\t\tFilter f1: " << f1.to_str());
+	create_filters();
 
-	Filter f2(tp_dst_port, 1234);
-	info_chatter(test_log, "\t\tFilter f2: " << f2.to_str());
-
-	Filter f3(ip_proto, 17);
-	info_chatter(test_log, "\t\tFilter f3: " << f3.to_str());
-
-	// uint32_t base_address = aton("10.0.0.0");
-	std::string port_pattern("65546");
-	Filter f4 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
-	info_chatter(test_log, "\t\tFilter f4: " << f4.to_str());
-
-//	std::string pattern("pattern - - 10.0.0.4 1024-10000 0 0");
-	std::string pattern("10.0.0.4");
-	Filter f5 = Filter::get_filter_from_ipclass_pattern(ip_dst, pattern);
-	info_chatter(test_log, "\t\tFilter f5: " << f5.to_str());
-
-	port_pattern = "1479-65457";
-	Filter f6 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
-	info_chatter(test_log, "\t\tFilter f6: " << f6.to_str());
-
-	pattern = "10.0.0.4/24";
-	Filter f7 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
-	info_chatter(test_log, "\t\tFilter f7: " << f7.to_str());
-
-	pattern = "255.255.255.255/1";
-	Filter f8 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
-	info_chatter(test_log, "\t\tFilter f8: " << f8.to_str());
-
-	port_pattern = "> 80";
-	Filter f9 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
-	info_chatter(test_log, "\t\tFilter f9: " << f9.to_str());
-
-	port_pattern = "!= 1000";
-	Filter f10 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
-	info_chatter(test_log, "\t\tFilter f10: " << f10.to_str());
-	info_chatter(test_log, "\t\tFilter f10 to IPCl: " << f10.to_ip_classifier_pattern());
+	intersect_filters();
 
 	std::cout << std::endl;
 	info_chatter(test_log, "Filter test is completed");
 
 	exit(SUCCESS);
+}
+
+void create_filters()
+{
+	info_chatter(test_log, "\t>>>>>>>>>>>>>> Create Filters");
+
+	Filter f1(ip_dst, aton("200.0.0.2"));
+	def_chatter(test_log, "\t\tFilter f1: " << f1.to_str());
+
+	Filter f2(tp_dst_port, 1234);
+	def_chatter(test_log, "\t\tFilter f2: " << f2.to_str());
+
+	Filter f3(ip_proto, 17);
+	def_chatter(test_log, "\t\tFilter f3: " << f3.to_str());
+	// uint32_t base_address = aton("10.0.0.0");
+	std::string port_pattern("65546");
+	Filter f4 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
+	def_chatter(test_log, "\t\tFilter f4: " << f4.to_str());
+
+//	std::string pattern("pattern - - 10.0.0.4 1024-10000 0 0");
+	std::string pattern("10.0.0.4");
+	Filter f5 = Filter::get_filter_from_ipclass_pattern(ip_dst, pattern);
+	def_chatter(test_log, "\t\tFilter f5: " << f5.to_str());
+
+	port_pattern = "1479-65457";
+	Filter f6 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
+	def_chatter(test_log, "\t\tFilter f6: " << f6.to_str());
+
+	pattern = "10.0.0.4/24";
+	Filter f7 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
+	def_chatter(test_log, "\t\tFilter f7: " << f7.to_str());
+
+	pattern = "255.255.255.255/1";
+	Filter f8 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
+	def_chatter(test_log, "\t\tFilter f8: " << f8.to_str());
+
+	port_pattern = "> 80";
+	Filter f9 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
+	def_chatter(test_log, "\t\tFilter f9: " << f9.to_str());
+
+	port_pattern = "!= 1000";
+	Filter f10 = Filter::get_filter_from_ipclass_pattern(tp_dst_port, port_pattern);
+	def_chatter(test_log, "\t\tFilter f10: " << f10.to_str());
+	def_chatter(test_log, "\t\tFilter f10 to IPCl: " << f10.to_ip_classifier_pattern());
+}
+
+void intersect_filters()
+{
+	std::cout << std::endl;
+	info_chatter(test_log, "\t>>>>>>>>>>>>>> Intersect Filters");
+
+	std::string pattern = "10.0.0.4/24";
+	Filter f1 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
+	def_chatter(test_log, "\t\tFilter f1: " << f1.to_str());
+
+	pattern = "192.168.0.1/24";
+	Filter f2 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
+	def_chatter(test_log, "\t\tFilter f2: " << f2.to_str());
+
+	f1.intersect(f2);
+	def_chatter(test_log, "\t\tIntersection of f1 Λ f2: " << f1.to_str());
+
+	pattern = "20.18.0.1/24";
+	Filter f3 = Filter::get_filter_from_v4_prefix_str(ip_src, pattern);
+	def_chatter(test_log, "\t\tFilter f3: " << f3.to_str());
+
+	f1.intersect(f3);
+	def_chatter(test_log, "\t\tIntersection of (f1Λf2) Λ f3: " << f1.to_str());
 }
